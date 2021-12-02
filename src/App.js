1984a0361c9e8b01ba3bc5ac3c584f5e456bf782
Martin Mwangi <mwangimartin1904@gmail.com>
2021-12-02 09:30:29 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Controlled from "./components/Controlled";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "400px", margin: "50px auto" }}>
+      <Controlled />
     </div>
   );
 }
---
+++ src/components/Controlled.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Controlled = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(email +  " - " + password);
+  };
+  return (
+    <form onSubmit={handleSubmit} autoComplete={false}>
+      <h4>Controlled</h4>
+      <div className="mb-3 mt-5">
+        <label className="form-label">Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Controlled;
